cb1770ef6ea81efb059a1e3cea2048ea1fbe2f85
Chaitanya Joglekar <chaitu25@users.noreply.github.com>
2021-11-18 08:12:41 +0000

    Update commands.sh

--- Data/commands.sh
+++ Data/commands.sh
@@ -1,5 +1,3 @@
-echo "Mapping artist and coreesponding trending songs on spotify"
+echo "Mapping artist and coreesponding trending songs on spotify along with Grammy Award Details"
 java -jar r2rml/r2rml.jar mappings/music-config.properties
 echo "-----------------------------------"
-echo "Mapping Grammy awards data"
-java -jar r2rml/r2rml.jar mappings/grammy-awards-config.properties
